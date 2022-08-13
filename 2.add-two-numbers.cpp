cdcb67af3384aabd5efa496e583ebb2e7d07fa42
Aayush <aayush5emails@gmail.com>
2022-08-13 16:52:04 +0530

    Add Two Numbers

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -20,35 +20,30 @@ class Solution
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
-
         ListNode *first = new ListNode;
         ListNode *last = first;
-
-        // Adding Nodes
-        int carry = 0;
+        int carry = 0, sum = 0;
         while (l1 != NULL || l2 != NULL || carry != 0)
         {
-            int sum = 0;
+            sum = 0;
             if (l1 != NULL)
             {
                 sum += l1->val;
                 l1 = l1->next;
             }
-
             if (l2 != NULL)
             {
                 sum += l2->val;
                 l2 = l2->next;
             }
-
             sum += carry;
             carry = sum / 10;
             ListNode *temp = new ListNode;
             temp->val = sum % 10;
+            temp->next = NULL;
             last->next = temp;
             last = temp;
         }
-
         return first->next;
     }
 };
