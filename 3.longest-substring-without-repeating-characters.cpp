2bcfca765b178f04bdce718c93ece3cf1dd7481d
Aayush <aayush5emails@gmail.com>
2022-08-14 15:13:05 +0530

    Longest substring without repeating characters

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -10,32 +10,30 @@ class Solution
 public:
     int lengthOfLongestSubstring(string s)
     {
-
         map<char, int> mpp;
         int left = 0, right = 0;
         int len = 0;
         while (right < s.length())
         {
             mpp[s[right]]++;
-            if (mpp.size() == right - left + 1)   // As substring has unique characters
-            {                                     // so the length of
-                len = max(len, right - left + 1); // substring should match window size
+            if (mpp.size() == right - left + 1)
+            {
+                len = max(len, right - left + 1);
             }
             else if (mpp.size() < right - left + 1)
             {
                 while (mpp.size() < right - left + 1)
                 {
                     mpp[s[left]]--;
-
                     if (mpp[s[left]] == 0)
+                    {
                         mpp.erase(s[left]);
-
+                    }
                     left++;
                 }
             }
             right++;
         }
-
         return len;
     }
 };
